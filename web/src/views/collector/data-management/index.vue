cdb8f7f2ffd12898910053d9d84344d7ab9e78f8
mooyang <mooyang@tencent.com>
2026-07-16 19:49:09 +0800

    style: tighten collector data view tabs

--- web/src/views/collector/data-management/index.vue
+++ web/src/views/collector/data-management/index.vue
@@ -78,8 +78,20 @@ watch(
 .management-content {
   min-height: 0;
   flex: 1;
-  margin-top: 16px;
+  margin-top: 12px;
   overflow: hidden;
+}
+
+.management-content :deep(.page-head) {
+  margin-bottom: 12px;
+}
+
+.management-content :deep(.collector-subtabs .arco-tabs-content) {
+  display: none;
+}
+
+.management-content :deep(.collector-subtabs .arco-tabs-tab:first-child) {
+  margin-left: 0;
 }
 
 .management-content :deep(.collector-workbench),
