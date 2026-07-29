500ce71429e0a0d4e6606708e3cb9cf24f861b3e
mooyang <mooyang@tencent.com>
2026-07-29 23:03:20 +0800

    fix(eventbus): avoid synchronous delivery acknowledgements

--- packages/jetstream/delivery_test.go
+++ packages/jetstream/delivery_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/nats-io/nats.go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -44,4 +45,23 @@ func TestDeliveryActionPreservesShorterCallerDeadline(t *testing.T) {
 	require.Error(t, err)
 	assert.True(t, errors.Is(err, context.DeadlineExceeded))
 	assert.Less(t, time.Since(started), 200*time.Millisecond)
+}
+
+func TestDeliveryAckDoesNotWaitForServerConfirmation(t *testing.T) {
+	srv, url := startTestServer(t)
+	defer srv.Shutdown()
+	conn, err := nats.Connect(url)
+	require.NoError(t, err)
+	defer conn.Close()
+
+	sub, err := conn.SubscribeSync("delivery.test")
+	require.NoError(t, err)
+	require.NoError(t, conn.PublishRequest("delivery.test", "missing.ack.responder", []byte("payload")))
+	msg, err := sub.NextMsg(time.Second)
+	require.NoError(t, err)
+
+	delivery := &Delivery{msg: msg, actionTimeout: 50 * time.Millisecond}
+	started := time.Now()
+	require.NoError(t, delivery.Ack(context.Background()))
+	assert.Less(t, time.Since(started), 50*time.Millisecond)
 }
